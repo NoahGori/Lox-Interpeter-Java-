3c96b262bb0fba21e793151ab3bee3c993847443
Noah Gori <darkilmap@gmail.com>
2020-12-22 21:10:30 -0600

    Parser is functional

--- lox/Lox.java
+++ lox/Lox.java
@@ -43,11 +43,14 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        
+        if(hadError) return;
 
-        for (Token token: tokens) {
-            System.out.println(token);
-        }
+        System.out.println(new AstPrinter().print(expression));
     }
+
     static void error(int line, String message) {
         report(line, "", message);
     }
@@ -55,5 +58,12 @@ public class Lox {
         System.err.println(
             "[line " + line + "] Error" + where + ": " + message);
             hadError = true;
+    }
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
